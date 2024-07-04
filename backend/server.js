563f08f326381d76f57012a4edda720224f80a4d
Roshan <roshan@racloop.com>
2024-07-04 10:32:01 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -15,6 +16,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
